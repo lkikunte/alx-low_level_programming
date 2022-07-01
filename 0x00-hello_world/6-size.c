7891dd115a78b1a72343d84b5b55ba79368201b3
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-06-30 19:11:25 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 /* Prints text to standard output*/
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long "
+"int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
 return (0);
 }
