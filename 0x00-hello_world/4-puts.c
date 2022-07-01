8476c6ef5b37237e5eba18eee53ee4f636908de5
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-06-30 18:24:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
 /**
- * main entry point
+ * main - entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-/* Prints text to standard output*/  
+/* Prints text to standard output*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
