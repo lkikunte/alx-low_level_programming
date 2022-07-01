dd1f599bfa4a331dace2afd0e1234c692a72c2dc
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-06-30 18:42:59 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
 int main(void)
 {
 /* Prints text to standard output*/
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
