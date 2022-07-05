db4ad447b34851b75a9698cb198805fa2dcdedd6
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-05 09:31:26 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-# include <stdio.h>
+# include "main.h"
 
 /**
 * main- Prints string  "_putchar" then quits
@@ -6,6 +6,14 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+printf("_");
+printf("p");
+printf("u");
+printf("t");
+printf("c");
+printf("h");
+printf("a");
+printf("r");
+printf("\n");
 return (0);
 }
