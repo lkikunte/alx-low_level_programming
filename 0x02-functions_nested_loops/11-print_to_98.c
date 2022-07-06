f113b36a0620c60a8525fcd51a6c26d0ad99ba9c
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-06 15:41:45 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - Prints all the numbers between a given number and 98.
+* @n: The number to be evaluated
+* Return: void
+*/
+void print_to_98(int n)
+{
+int d;
+
+d = n - 98;
+if (d < 0)
+{
+	for (n; n < 98; n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	printf("98");
+}
+else
+	for (n; n > 98; n--)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	printf("98");
+
+return;
+}
