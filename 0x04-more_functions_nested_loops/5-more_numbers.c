c75f066f7f25ff2381fd001230ed04b4c76570d7
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-08 16:14:48 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* more_numbers - Prints the numbers 0 to 14, 10 times
+* Return: void
+*/
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
