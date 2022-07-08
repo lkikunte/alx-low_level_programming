787048679701ae660818218d878f363fa0326376
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-08 16:28:12 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - Draws as many lines as determined by variable
+* @n: Determines how many lines should be drawn
+* Return: void
+*/
+void print_line(int n)
+{
+	int x;
+
+	if (n < 1)
+		_putchar('\n');
+	else
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
+}
