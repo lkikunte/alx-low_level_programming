465160ac67957deabb618849ec9f21b474a5a129
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-08 17:31:25 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_diagonal - Draws a diagonal line across screen
+* @n: The length of diagonal line
+* Return: void
+*/
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+			_putchar('32';
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_square - Prints squares n times
+* @size: The number of times square is printed
+* Return: void
+*/
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
