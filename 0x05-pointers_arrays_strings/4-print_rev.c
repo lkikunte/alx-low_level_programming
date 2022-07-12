571dde95e7264f3a210aba7cf2a418fcdcd6fae3
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-12 15:40:44 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - Prints a given string in reverse
+* @s: The string to be printed
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	for (c -= 1; c >= 0; c--)
+		_putchar(s[c]);
+	_putchar('\n');
+}
