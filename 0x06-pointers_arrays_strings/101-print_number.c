dc676d30252094b52ce3f2cb48721e0bf00a01aa
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-07-14 15:00:45 -0700

    Project 0x06

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_number - Prints any integer
+* @n: Number to print
+* Return: void
+*/
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+		{
+		_putchar('-');
+		n *= -1;
+		}
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
+}
