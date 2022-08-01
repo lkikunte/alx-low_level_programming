303512e396d01bcafc1ecb3dad6830784fedaff7
Lukindo Kikunt'e <lkikunte@gmail.com>
2022-08-01 16:32:41 -0700

    Project 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+* struct dog - decription of the dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog.
+* Description: description the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
